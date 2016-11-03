c21f336539f644a624af977fa453859b99f4c75d
Garrett Tan <garrett.t.tan@gmail.com>
2016-11-02 18:11:57 -0700

    bl;askdjfl;sjd

--- projects/proj3_measurement/rtts.py
+++ projects/proj3_measurement/rtts.py
@@ -1,7 +1,6 @@
 """A python script that pings servers and records/plots the rtts and drop rate"""
 import subprocess
-
-
+from collections import defaultdict
 
 """ Runs ping commands and generates json output
 	hostnames: list of hosts to ping
@@ -10,24 +9,37 @@ import subprocess
 	aggregated_ping_output_filename: name of file to output the aggregated ping results to """
 
 def run_ping(hostnames, num_packets, raw_ping_output_filename, aggregated_ping_output_filename):
+	rtts = dict()
 	for host in hostnames:
 		# Ping host num_packets times
 		output = subprocess.check_output(["ping", "-c", str(num_packets), host])
 		# Go though the lines of output and write to raw_ping_output_filename
 		# Timeouts should have rtt = -1
-		
-		# Aggregate data is in the last 2 lines of output anyway, 
-		# so we can just use this instead of keeping track while we are going through the lines of output
+		lines = output.split('\n')[1:]
+		rtts[host] = [-1] * num_packets
+		for line in lines:
+			print line
+			if not line:
+				break
+			elif 'bytes from' not in line:
+				continue
+			else:
+				index = int(line.split()[4].split('=')[1])
+				rtts[host][index] = float(line.split()[6].split('=')[1])
 
+		# Aggregate data is in the last 2 lines of output anyway,
+		# so we can just use this instead of keeping track while we are going through the lines of output
+	return rtts
 
 def plot_median_rtt_cdf(agg_ping_results_filename, output_cdf_filename):
-
+	return None
 
 
 def plot_ping_cdf(raw_ping_results_filename, output_cdf_filename):
+	return None
 
 
-
-
-
-
+if __name__ == '__main__':
+	out = run_ping(['google.com'], 30, None, None)
+	print len(out['google.com'])
+	print out
