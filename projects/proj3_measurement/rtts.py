aad1e264cdacf89eb823ad2e9edfbb21915ec0fa
Aditi Ganpule <aditiganpule@airbears2-10-142-30-64.airbears2.1918.berkeley.edu>
2016-11-06 15:06:12 -0800

    part b results & 1 extra ping

--- projects/proj3_measurement/rtts.py
+++ projects/proj3_measurement/rtts.py
@@ -16,7 +16,7 @@ def run_ping(hostnames, num_packets, raw_ping_output_filename, aggregated_ping_o
 		host = host.strip()
 		print host
 		try:
-			output = subprocess.check_output(["ping", "-c", str(num_packets), host])
+			output = subprocess.check_output(["ping", "-c", str(num_packets + 1), host])
 			p = re.compile('\d+ bytes from \d+\.\d+\.\d+\.\d+: icmp_seq=(\d+) ttl=\d+ time=(\d+\.\d+) ms')
 			raw_pings[host] = [-1.0] * num_packets
 			pings = p.findall(output)
@@ -42,6 +42,7 @@ def run_ping(hostnames, num_packets, raw_ping_output_filename, aggregated_ping_o
 		json.dump(aggregate_pings, f)
 
 def plot_median_rtt_cdf(agg_ping_results_filename, output_cdf_filename):
+
 	return None
 
 
@@ -53,3 +54,4 @@ if __name__ == '__main__':
 	with open('alexa_top_100', 'r') as f:
 		# run_ping(f.readlines(), 10, 'rtt_a_raw.json', 'rtt_a_agg.json')
 		run_ping(['google.com', 'todayhumor.co.kr', 'zanvarsity.ac.tz', 'taobao.com'], 500, 'rtt_b_raw.json', 'rtt_b_agg.json')
+
