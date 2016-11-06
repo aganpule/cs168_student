ff0e5af7ae8fbc9064dd158dcc243c99362a365d
Garrett Tan <garrett.t.tan@gmail.com>
2016-11-06 12:05:37 -0800

    yayayayay

--- projects/proj3_measurement/rtts.py
+++ projects/proj3_measurement/rtts.py
@@ -3,22 +3,17 @@ import subprocess
 import re
 from numpy import median
 import json
-
-""" Runs ping commands and generates json output
-	hostnames: list of hosts to ping
-	num_packets: number of packets to send to each host
-	raw_ping_output_filename: name of file to output the raw ping results to
-	aggregated_ping_output_filename: name of file to output the aggregated ping results to """
+import pprint
 
 def run_ping(hostnames, num_packets, raw_ping_output_filename, aggregated_ping_output_filename):
 	raw_pings, aggregate_pings = dict(), dict()
 	for host in hostnames:
 		host = host.strip()
+		raw_pings[host] = [-1.0] * num_packets
 		print host
 		try:
 			output = subprocess.check_output(["ping", "-c", str(num_packets), host])
 			p = re.compile('\d+ bytes from \d+\.\d+\.\d+\.\d+: icmp_seq=(\d+) ttl=\d+ time=(\d+\.\d+) ms')
-			raw_pings[host] = [-1.0] * num_packets
 			pings = p.findall(output)
 			successes = []
 			for seq_no, time in pings:
@@ -42,14 +37,29 @@ def run_ping(hostnames, num_packets, raw_ping_output_filename, aggregated_ping_o
 		json.dump(aggregate_pings, f)
 
 def plot_median_rtt_cdf(agg_ping_results_filename, output_cdf_filename):
-	return None
+	with open('output/' + agg_ping_results_filename, 'r') as f:
+		aggregate_pings = json.load(f)
+		medians = sorted([aggregate_pings[site]['median_rtt'] for site in aggregate_pings])
+		pprint.pprint(medians)
+		print sum([1 if n == -1.0 else 0 for n in medians])
 
+def find_num_no_response():
+	with open('output/rtt_a_agg.json', 'r') as f:
+		aggregate_pings = json.load(f)
+		return sum([1 if aggregate_pings[site]['median_rtt'] == -1.0 else 0 for site in aggregate_pings])
+
+def find_num_at_least_one_failure():
+	with open('output/rtt_a_raw.json', 'r') as f:
+		raw_pings = json.load(f)
+		return sum([1 if -1.0 in raw_pings[site] else 0 for site in raw_pings])
 
 def plot_ping_cdf(raw_ping_results_filename, output_cdf_filename):
-	return None
+	with open('output/' + raw_ping_results_filename, 'r') as f:
+		raw_pings = json.load(f)
 
 
 if __name__ == '__main__':
-	with open('alexa_top_100', 'r') as f:
-		# run_ping(f.readlines(), 10, 'rtt_a_raw.json', 'rtt_a_agg.json')
-		run_ping(['google.com', 'todayhumor.co.kr', 'zanvarsity.ac.tz', 'taobao.com'], 500, 'rtt_b_raw.json', 'rtt_b_agg.json')
+	# with open('alexa_top_100', 'r') as f:
+	# 	run_ping(f.readlines(), 10, 'rtt_a_raw.json', 'rtt_a_agg.json')
+		# run_ping(['google.com', 'todayhumor.co.kr', 'zanvarsity.ac.tz', 'taobao.com'], 500, 'rtt_b_raw.json', 'rtt_b_agg.json')
+	# plot_median_rtt_cdf('rtt_a_agg.json', None)
